71e1f0435a28bd3088b66941b9d06cf6f997fad9
Abel Ilito <ilitoabel@gmail.com>
2020-12-21 09:41:05 +0100

    Navbar part 1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,4 @@ require("components/button.scss")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "@fortawesome/fontawesome-free/js/all";
